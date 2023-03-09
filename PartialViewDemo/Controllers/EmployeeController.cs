9d1409cefedd894fd7facc29dc8fc0096cb01d93
KrushnaBodake <bodakekrushna9@gmail.com>
2023-03-09 17:54:44 +0530

    Add project files.

---
+++ PartialViewDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewDemo.Data;
+
+namespace PartialViewDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public EmployeeController(ApplicationDbContext context)
+        {
+                this.context = context;
+        }
+        public IActionResult Index()
+        {
+           var emp= context.Employees.ToList();
+            return View(emp);
+        }
+    }
+}
---
+++ PartialViewDemo/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartialViewDemo.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int EmpId { get; set; }
+        [Required]
+        public string? Empname { get; set; }
+        [Required]
+        public string? Deptname { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
